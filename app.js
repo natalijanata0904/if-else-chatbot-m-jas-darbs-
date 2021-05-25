e3f1911273ac43e77959430cd88d23a252df2eef
natalijanata0904 <82841763+natalijanata0904@users.noreply.github.com>
2021-05-25 22:09:41 +0300

    Add files via upload

---
+++ app.js
@@ -0,0 +1,17 @@
+let p = document.getElementbyId('p');
+
+let btn = document.getElementbyId('btn');
+
+
+function funk() {
+    p.innerHTML = "Hello";
+    p.style.fontSize= "30px";
+}
+
+let user = {
+    firstname: "John",
+    lastname: "Smith",
+}
+
+alert(user.firstname);
+
